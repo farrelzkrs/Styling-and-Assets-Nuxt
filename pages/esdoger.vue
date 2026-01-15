ae18a5b9beedb3c9811bf656323408e23e152979
farrelzkrs <farrel.z.suryahadi@gmail.com>
2026-01-15 15:06:10 +0700

    add jeli

--- pages/esdoger.vue
+++ pages/esdoger.vue
@@ -1,18 +1,23 @@
 <script lang="ts" setup>
-import Jeli from './esdoger/jeli.vue';
+import Jeli from "./esdoger/jeli.vue";
 
-
-  useSeoMeta({
-      title: "Es Doger",
-      description: "Ini adalah page orang kehausan",
-  })
+useSeoMeta({
+  title: "Es Doger",
+  description: "Ini adalah page orang kehausan",
+});
 </script>
 <template>
   <div>
-    <h1 style="margin-bottom: 12px; text-align: center;">Ini Es Doger - Parent</h1>
+    <h1 style="margin-bottom: 12px; text-align: center">Ini Es Doger - Parent</h1>
     <NuxtPage />
-    <v-divider color="secondary" opacity=".7" thickness="5" variant="dashed" gradient></v-divider>
-    <h1 style="margin-top: 12px; text-align: center;">Ini Pakai Topping</h1>
+    <v-divider
+      color="secondary"
+      opacity=".7"
+      thickness="5"
+      variant="dashed"
+      gradient
+    ></v-divider>
+    <h1 style="margin-top: 12px; text-align: center">Ini Pakai Topping</h1>
     <Jeli />
   </div>
 </template>
