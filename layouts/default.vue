b7d70726cd9b38cd28fb7ec83c242bb6211419ff
farrelzkrs <farrel.z.suryahadi@gmail.com>
2026-01-19 12:12:12 +0700

    bottom navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,11 @@ import { ref } from "vue";
 
 const navbarStyle = ref<"terang" | "hytam">("terang");
 
+const active = ref(false);
+const toggleBottomNav = () => {
+  active.value = !active.value;
+};
+
 const toggleNavbarStyle = () => {
   navbarStyle.value = navbarStyle.value === "terang" ? "hytam" : "terang";
 };
@@ -157,6 +162,21 @@ const toggleNavbarStyle = () => {
       </header>
       <v-main>
         <slot />
+        <v-btn
+          class="fab-toggle"
+          color="yellow"
+          icon
+          elevation="8"
+          @click="toggleBottomNav"
+        >
+          <v-icon>{{ active ? "mdi-minus" : "mdi-plus" }}</v-icon>
+        </v-btn>
+        <v-bottom-navigation v-if="active" color="primary" class="custom-bottom-nav" app>
+          <v-btn to="/" icon="mdi-home" />
+          <v-btn to="/esdoger" icon="mdi-cup" />
+          <v-btn to="/about" icon="mdi-information-outline" />
+          <v-btn to="/contact" icon="mdi-contacts" />
+        </v-bottom-navigation>
         <footer style="text-align: center; margin-top: 20px">&copy; Farrel 2023</footer>
       </v-main>
     </v-app>
